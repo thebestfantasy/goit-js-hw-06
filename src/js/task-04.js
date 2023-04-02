e573d54e1d4e618160b93513283d2a2beb61a866
thebestfantasy <sergej.fronch@gmail.com>
2023-04-02 22:46:26 +0300

    endddd

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,5 +1,5 @@
-const incrementBtn = document.querySelector('button[data-action="increment"]')
-const decrementBtn = document.querySelector('button[data-action="decrement"]')
+const incrementBtn = document.querySelector('[data-action="increment"]')
+const decrementBtn = document.querySelector('[data-action="decrement"]')
 const value = document.querySelector('#value');
 
 decrementBtn.addEventListener("click", ofClick)
@@ -9,12 +9,12 @@ let counterValue = 0;
 
 function onClick() {
     counterValue += 1;
-    value.textContent = `${counterValue}`;
+    value.textContent = counterValue;
 }
 
 function ofClick(event) {
     counterValue -= 1;
-    value.textContent = `${counterValue}`;
+    value.textContent = counterValue;
     console.log(event.currentTarget);
 }
 
