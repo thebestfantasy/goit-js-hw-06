79c9ece3bbf7c6de5227bd503f9d30d1bed6797d
thebestfantasy <sergej.fronch@gmail.com>
2023-04-05 11:13:48 +0300

    add task 10

--- js/task-10.js
+++ js/task-10.js
@@ -4,33 +4,35 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
-// Напиши скрипт створення і очищення колекції елементів. Користувач вводить кількість елементів в input і натискає кнопку Створити, після чого рендериться колекція.
-// Натисненням на кнопку Очистити, колекція елементів очищається.
+const createBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-destroy]");
+const input = document.querySelector("#controls").firstElementChild;
+const mainDiv = document.querySelector("#boxes");
+createBtn.addEventListener("click", createBoxes);
+destroyBtn.addEventListener("click", deleteBoxes);
+input.addEventListener("input", onInput);
 
-// <div id="controls">
-//   <input type="number" min="1" max="100" step="1" />
-//   <button type="button" data-create>Create</button>
-//   <button type="button" data-destroy>Destroy</button>
-// </div>
+function onInput() {
+  const amount = parseInt(input.value);
+  createBoxes(amount);
+}
 
-// <div id="boxes"></div>
+function createBoxes(amount) {
+  let size = 30;
+  const boxes = [];
+  for (let i = 0; i < amount; i += 1) {
+    const div = document.createElement("div");
+    div.textContent = "box";
+    div.style.outline = "1px solid black";
+    size += 10;
+    div.style.height = `${size}px`;
+    div.style.width = `${size}px`;
+    div.style.backgroundColor = getRandomHexColor();
+    boxes.push(div);
+  }
+  mainDiv.append(...boxes);
+}
 
-// Створи функцію createBoxes(amount), яка приймає один параметр - число. Функція створює стільки <div>, скільки вказано в amount і додає їх у div#boxes.
-
-//     Розміри найпершого <div> - 30px на 30px.
-//     Кожен елемент після першого повинен бути ширшим і вищим від попереднього на 10px.
-//     Всі елементи повинні мати випадковий колір фону у форматі HEX. Використовуй готову функцію getRandomHexColor для отримання кольору.
-
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215)
-//     .toString(16)
-//     .padStart(6, 0)}`;
-// }
-
-// Створи функцію destroyBoxes(), яка очищає вміст div#boxes, у такий спосіб видаляючи всі створені елементи.
-
-const createBtn = document.querySelector('[data-create]');
-const destroyBtn = document.querySelector('[data-destroy]');
-const inp = document.querySelector("#controls");
-const input = inp.firstElementChild;
-console.log(input);
+function deleteBoxes() {
+  mainDiv.innerHTML = "";
+}
